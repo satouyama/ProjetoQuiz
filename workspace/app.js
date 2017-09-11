c39aaa3d3aa9616306c5fc6f6fc7359f8e70685b
Douglas <senhorsatou@gmail.com>
2017-09-11 14:25:34 -0300

    Adicionando tela de ajuda

--- workspace/app.js
+++ workspace/app.js
@@ -14,33 +14,41 @@ app.set('view engine','ejs');
 //set views directory
 app.set('views','./public/views');
 
-// router login
+// router first game
 app.get('/Quiz1', function (req, res) {
 //  res.send('works fine')
  res.render('jogos/jogo'); //the new game ejs
 });
-
+//router second game
 app.get('/Quiz2',function(req, res){
 
   res.render('jogos/jogo2');
 });
 
+//router login page
 app.get('/', function(req, res){
 
    res.render('index/index');
 
 });
 
+//router register
 app.get('/cadastro', function(req, res){
   res.render('cadastro/cadastro');
 });
 
+//router dashbord for information
 app.get('/dashbord', function(req, res){
  res.render('jogos/dashbord');
 });
 
+//router game list
 app.get('/gamelist', function(req,res){
  res.render('jogos/GameList');
 });
+
+app.get('/ajuda',function(req, res){
+ res.render('jogos/ajuda');
+})
 
 module.exports = app;
